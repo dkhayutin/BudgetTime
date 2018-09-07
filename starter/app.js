9b738ca5bb01c14ef9f92872e5653575401d660b
Dan Khayutin <dan.khayutin@gmail.com>
2018-09-07 12:05:17 -0400

    update percentage totals when expense is added

--- starter/app.js
+++ starter/app.js
@@ -6,7 +6,21 @@ var budgetController = (function () {
     this.id = id;
     this.description = description;
     this.value = value;
+    this.percentage = -1;
   };
+
+  Expense.prototype.calcPercentage = function(totalIncome) {
+    if(totalIncome > 0) {
+      this.percentage = Math.round((this.value / totalIncome) * 100);
+    } else {
+      this.percentage = -1;
+    }
+
+  };
+
+  Expense.prototype.getPercentage = function() {
+    return this.percentage;
+  }
 
   var Income = function(id, description, value) {
     this.id = id;
@@ -91,6 +105,21 @@ var budgetController = (function () {
         data.percentage = -1;
       }
 
+    },
+
+    calculatePercentages: function() {
+
+      data.allItems.exp.forEach(function(cur) {
+        cur.calcPercentage(data.totals.inc);
+      });
+
+    },
+
+    getPercentages: function() {
+      var allPerc = data.allItems.exp.map(function(cur) {
+        return cur.getPercentage()
+      });
+      return allPerc;
     },
 
     getBudget: function() {
@@ -236,9 +265,15 @@ var controller = (function (budgetCtrl, UICntrl) {
 
     // 1. Calculate percentages
 
+    budgetCtrl.calculatePercentages();
+
     // 2. Read percentages from the budget controller
 
+    var percentages = budgetCtrl.getPercentages()
+
     // 3. Update UI with new percentages
+
+    console.log(percentages);
   };
 
   var ctrlAddItem = function () {
